b69f970c35cf1057ec2715044099145f8a55ff0c
liu tao <465275024@qq.com>
2018-11-12 17:41:37 +0800

    完成全部分类页

---
+++ src/components/classtify-item.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="classtify-item">
+        <p v-for="(item,index) in nameArr" :key="index">
+            {{ item }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['nameArr'],
+        data() {
+            return {
+                // key: value
+            }
+        },
+    }
+</script>
+
+<style lang='scss' scoped>
+.classtify-item{
+    display: flex;
+    // flex-direction: row;
+    flex-wrap: wrap;
+    box-sizing: border-box;
+    p{
+        background-color: white;
+        box-sizing: border-box;
+        border: 1px solid #eee;
+        margin: 0px;
+        width: 25vw;
+        height: 10vw;
+        line-height: 10vw;
+        color: #666;
+        font-size: 4vw;
+    }
+}
+</style>
