a7ad5a6baedac93d3e9515ec4be3a02b05132cad
liu tao <465275024@qq.com>
2018-11-12 15:10:41 +0800

    去掉header右边按钮

--- src/views/ClasstifyAll.vue
+++ src/views/ClasstifyAll.vue
@@ -3,7 +3,7 @@
     <div>
         <mt-header title="全部分类">
             <mt-button icon="back" slot="left" @click="$router.go(-1)">返回</mt-button>
-            <!-- <mt-button icon="more" slot="right"></mt-button> -->
+            <mt-button icon="more" slot="right"></mt-button>
         </mt-header>
         全部的分类
     </div>
