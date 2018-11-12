9824598fcb8aceda12045074fd23d544aee99daf
liu tao <465275024@qq.com>
2018-11-12 23:53:22 +0800

    1.更新iconfont  2.增加搜索页

---
+++ src/components/search-bar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="root">
+            <router-link to="/search">
+            <i class="iconfont icon-sousuo"></i>
+            <span>搜索bar</span>
+            </router-link>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+/**
+ * 搜索条，点击后跳转到search.vue
+ */
+    export default {
+        data:function() {
+            return {
+                // key: value
+            }
+        },
+
+    }
+</script>
+
+<style lang='scss' scoped>
+.root{
+    // border: 1px solid #ddd;
+    font-size: 4vw;
+    text-align: center;
+    color: #999;
+    background-color: #fff;
+    width: 60vw;
+    height: 8vw;
+    line-height: 8vw;
+    border-radius: 4vw;
+    a{
+        color: #999;
+        text-decoration: none;
+    }
+}
+</style>
---
+++ src/views/search.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        搜索页
+        <mt-header  title="全部分类">
+                <mt-button icon="back" slot="left" @click="$router.go(-1)">返回</mt-button>
+                <!-- <mt-button icon="more" slot="right"></mt-button> -->
+            </mt-header>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
