7c9ac0932b9ef101a8f23dcd2928785abcb09f2f
liu tao <465275024@qq.com>
2018-11-13 15:19:32 +0800

    1.调整lt-header对齐2.修改search页

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,19 +1,46 @@
 <template>
     <div>
-        搜索页
-        <mt-header  title="全部分类">
-                <mt-button icon="back" slot="left" @click="$router.go(-1)">返回</mt-button>
-                <!-- <mt-button icon="more" slot="right"></mt-button> -->
-            </mt-header>
+        <lt-header>
+            <div class="root">
+                <i class="iconfont icon-sousuo"></i>
+                <input type="text">
+            </div>
+        </lt-header>
     </div>
 </template>
 
 <script>
+import ltHeader from '../components/lt-header'
     export default {
-        
+        components:{
+            'lt-header':ltHeader
+        },
+        data:function() {
+            return {
+                // key: value
+            }
+        },
     }
 </script>
 
-<style scoped>
-
+<style lang='scss' scoped>
+.root{
+    // border: 1px solid #ddd;
+    font-size: 4vw;
+    text-align: center;
+    color: #999;
+    background-color: #fff;
+    width: 60vw;
+    height: 8vw;
+    line-height: 8vw;
+    border-radius: 4vw;
+    a{
+        color: #999;
+        text-decoration: none;
+    }
+    input{
+        border: none;
+        outline: none;
+    }
+}
 </style>
