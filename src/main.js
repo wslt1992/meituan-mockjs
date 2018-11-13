e6dfcf01e830839d435ebc5cd330f6b51342412b
liu tao <465275024@qq.com>
2018-11-13 15:44:29 +0800

    1.url加入全局this.$url 2.增加back-button

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import url from './global/url'
 import './style/style.css'
 import './assets/iconfont/iconfont.css'
 Vue.config.productionTip = false
 
 import './mockjs/app-mock';
+
+Vue.prototype.$url = url;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
