abe953357636eb2217696fe73784d6f7db54e12c
liu tao <465275024@qq.com>
2018-11-16 14:47:45 +0800

    加入新组建上下、页、左右

---
+++ src/components/t-page.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="t-page-root">
+        <div name='header'>
+            <slot name='header'></slot>
+        </div>
+        <div name='container'>
+            <slot name='container'></slot>
+        </div>
+        <div class="footer">
+            <slot name='footer'></slot>
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // key: value
+            }
+        },
+    }
+</script>
+
+<style lang='scss' scoped>
+.t-page-root{
+    display: flex;
+    flex-direction: column;
+    .header{
+        height: 20vw;
+        width: 100%;
+    }
+    .container{
+        flex-grow: 1;
+    }
+    .footer{
+        height: 20vw;
+        width: 100%;
+    }
+
+}
+    
+</style>
