69b0322408c1dedb48ddc9207fe9a18119b68fd8
liu tao <465275024@qq.com>
2018-11-17 23:33:21 +0800

    加入t-icon

---
+++ src/components/t-icon.vue
@@ -0,0 +1,38 @@
+<template>
+        <div >
+            <i :style="linearColor" :class="iconname">
+            </i>
+        </div>
+</template>
+
+<script>
+export default {
+    props:{
+        startColor:{
+            default:'#000',
+        },
+        endColor:{
+            default:'#fff',
+        },
+        'angle':{
+            default:`90deg`
+        },
+        iconname:{
+            default:'iconfont icon-QQ'
+        }
+    },
+   
+    computed : {
+        linearColor() {
+            return {
+                'background-image':`linear-gradient(135deg,${this.startColor},${this.endColor})`
+                }
+        }
+        
+    },
+}
+</script>
+
+<style scoped>
+.icon-QQ {-webkit-background-clip:text;-webkit-text-fill-color: transparent;display: inline-block;font-size: 5vw;}
+</style>
