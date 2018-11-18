7059ef5e9bf8e06487b3bab3e1460bd5868605ed
liu tao <465275024@qq.com>
2018-11-18 17:24:23 +0800

    1.开始写order订单2.修改page.vue3.提取search-result中的list-item

---
+++ src/components/t-header-new.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="t-header">
+        <div class="left">
+            <slot name="left">
+                <t-back-button v-if="back"><i class="icon iconfont icon-arrowleft"></i></t-back-button>
+            </slot>
+        </div>
+        <div class="center">
+            <slot name="center" class="center"></slot>
+        </div>
+        <div class="right">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+/**
+ *  props:back:true 默认具有返回按钮
+ */
+export default {
+  props: {
+    back: {
+      default: true
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+.t-header {
+  display: flex;
+  justify-content: space-between;
+  padding-top: 2vw;
+  padding-bottom: 2vw;
+  background-color: #28bb9b;
+  .left{
+      display: flex;
+        align-items: center;
+    i{
+        font-size: 6vw;
+    }
+  } 
+  .right{
+    //   width: 5vw;
+    
+  }
+  .center {
+    flex-grow: 1;
+    
+  }
+}
+// .icon-arrowleff:before { content: "\e90e"; }
+</style>
--- src/views/search-result.vue
+++ src/views/search-result.vue
@@ -98,11 +98,14 @@
         v-infinite-scroll="loadMore"
         infinite-scroll-disabled="loading"
         infinite-scroll-distance="50">
-            <div v-for="(item,index) in items" :key="index" class="list-item">
+            <search-list-item :items='items'> 
+
+            </search-list-item>
+            <!-- <div v-for="(item,index) in items" :key="index" class="list-item">
                 <h6>{{item.id}}</h6>
-                <div class="item-left">
+                <div class="item-left"> -->
                     <!-- <img :src='item.img' alt=""> -->
-                    <img src="../assets/imgs/shop.jpg" alt="">
+                    <!-- <img src="../assets/imgs/shop.jpg" alt="">
                 </div>
                 <div class="item-right">
                     <span class="shopname">{{item.shopname}}</span>
@@ -116,7 +119,7 @@
                     </div>
                     <span class="address">地址:{{item.tips}}</span>
                 </div>
-            </div>
+            </div> -->
             <mt-spinner type="triple-bounce"></mt-spinner>
         </div>
     </div>
@@ -146,6 +149,7 @@
             requestData(){
                 this.$https.listing.request().then((data)=>{
                     this.items.push(...data.list)
+                    console.log(this.items)
                 })
             },
             switchBottom(num){
@@ -202,59 +206,5 @@
     }
     
     // 分类bar结束
-    // 列表开始
-    .list-item{
-        display: flex;
-        flex-direction: row;
-        text-align: start;
-        font-size: 4.3vw;
-        margin: 2vw 0;
-        .item-left{
-            img {
-                width: 25vw;
-                height: 25vw;
-            }
-        }
-        .item-right{
-            flex-grow: 1;
-
-            display: flex;
-            flex-direction: column;
-            >*{
-                margin: 0.6vw 0;
-            }
-            .shopname{
-                font-size: 5.5vw;
-                font-weight: bold;
-                color: #222;
-            }
-            .div-item{
-                display: flex;
-                justify-content: space-between;
-                .price{
-                    color: #666;
-                }
-                .distance{
-                    color: #aaa;
-                }
-                .tips{
-                    width: 40vw;
-                    color: #aaa;
-                    overflow: hidden;    
-                    text-overflow:ellipsis;    
-                    white-space: nowrap;
-                }
-                .sold{
-                    color: aquamarine;
-                }
-                
-            }
-            .address{
-                font-size: 3.6vw;
-            }
-            
-            
-        }
-    }
-    //列表结束
+    
 </style>
