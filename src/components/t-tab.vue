6de08df031974c1e81e9d41e9fa9d6c909a8eddd
liu tao <465275024@qq.com>
2018-11-19 04:14:06 +0800

    加入t-tab.vue

---
+++ src/components/t-tab.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="t-tab-root">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+/**
+ * 使用案例
+    <t-tab checked-clazz='active'><div >456</div><div >456</div></t-tab>
+ * 
+    .active{
+    color: red;
+    }
+ */
+    const checkedClazz='checked-clazz'
+    export default {
+        data() {
+            return {
+                //全部的item
+                tabItemS:[],
+                currentCheckedNum:0,
+                /**
+                 * 被选中后的样式名，样式需在父定义
+                 * 通过 属性方式传入例如：<t-tab checked-clazz='active'>
+                 */
+                checkedClass:''
+            }
+        },
+        methods: {
+            changeAddClass(newValue,oldValue) {
+                this.tabItemS[oldValue].classList.remove(this.checkedClass);
+                this.tabItemS[newValue].classList.add(this.checkedClass);
+            },
+            chageEmit(newValue){
+                this.$emit('input',newValue)
+            }
+        },
+        mounted(){
+            this.tabItemS = this.$el.querySelectorAll('.t-tab-root>*');
+            console.log(this.tabItemS)
+            this.tabItemS.forEach((element,index) => {
+                element.addEventListener('click',()=>{
+                    this.currentCheckedNum = index;
+                    console.log(this.currentCheckedNum,22222222222)
+                })
+            });
+            this.checkedClass = this.$el.getAttribute(checkedClazz)
+            console.log('checkedClass111111111',this.checkedClass)
+        },
+        watch: {
+            currentCheckedNum(newValue, oldValue) {
+                this.changeAddClass(newValue,oldValue);
+                this.chageEmit(newValue)
+            }
+        },
+    }
+</script>
+
+<style lang='scss' scoped>
+    .t-tab-root{
+        display: flex;
+        >*{
+            flex-grow: 1;
+        }
+    }
+</style>
