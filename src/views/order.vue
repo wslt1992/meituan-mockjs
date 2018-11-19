d52436a321864608422fc3ca931a3f73254dfe34
liu tao <465275024@qq.com>
2018-11-19 15:27:30 +0800

    order.vue完成

--- src/views/order.vue
+++ src/views/order.vue
@@ -5,57 +5,274 @@
         </t-header-new>
         <div slot="container">
             <t-tab checked-clazz='active' class="tab" v-model="selectedIndex">
-                <div >123</div>
-                <div>456</div>
-                <div>456</div>
-                <div>456</div>
-                <div>456</div>
-                <div>456</div>
-                <div>456</div>
-                <div>456</div>
-                <div>456</div>
+                <div>全部</div>
+                <div>待付款</div>
+                <div>待使用</div>
+                <div>待评价</div>
+                <div>退款/售后</div>
             </t-tab>
+            <order-item :items='items' ></order-item>
         </div>
     </t-page>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            selectedIndex: 0
+  data() {
+    return {
+      selectedIndex: 0,
+      items:[],
+      itemsAll: [
+        {
+          shopname: "正新鸡排",
+          state: "已经完成",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "已经完成",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "已经完成",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "已经完成",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "已经完成",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "已经完成",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+      ],
+      /**
+       * 未付款
+       */
+      itemsNoPayments:[
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未付款",
+          price: 234,
+          time: "2018-03-24 18:34"
         }
-    },
-}
+      ],
+      /**
+       * 未使用
+       */
+      itemsNoEat:[
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+        {
+          shopname: "正新鸡排",
+          state: "未使用",
+          price: 234,
+          time: "2018-03-24 18:34"
+        }
+      ],
+      /**
+       * 未评论
+       */
+      itemsNoComment:[
+        {
+          shopname: "正新鸡排",
+          state: "未评论",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+      ],
+      /**
+       * 售后
+       */
+      itemsNoAfterSales:[
+        {
+          shopname: "正新鸡排",
+          state: "售后",
+          price: 234,
+          time: "2018-03-24 18:34"
+        },
+      ]
+    };
+  },
+  methods:{
+      /**
+       * 切换tab内容
+       */
+      switchTabContainer(index){
+          switch(index){
+              case 0:
+                this.items = this.itemsAll
+                break;
+              case 1:
+                this.items = this.itemsNoPayments
+                break;
+              case 2:
+                this.items =this.itemsNoEat
+                break;
+              case 3:
+                this.items =this.itemsNoComment
+                break;
+              case 4:
+                this.items =this.itemsNoAfterSales
+                break;
+          }
+      }
+  },
+  watch: {
+      selectedIndex(newValue, oldValue) {
+          this.switchTabContainer(newValue);
+      }
+  },
+};
 </script>
 
 <style lang='scss' scoped>
-@import '../style/color.scss';
-.header{
-    font-size: 5vw;
-    height: 10vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+@import "../style/color.scss";
+.header {
+  font-size: 5vw;
+  height: 10vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.container{
-    // tab开始
-    .tab{
-        div{
-            height: 10vw;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            box-sizing: border-box;
-            border-bottom: 0.3vw solid $gray;
-            padding-bottom: 0.4vw;
-        }
-        div.active{
-            border-bottom: 0.5vw solid $primary-color;
-            color: $primary-color;
-            padding-bottom: 0vw;
-        }
+.container {
+  // tab开始
+  .tab {
+    div {
+      height: 10vw;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+      border-bottom: 0.3vw solid $gray;
+      padding-bottom: 0.4vw;
     }
-    // tab结束
+    div.active {
+      border-bottom: 0.5vw solid $primary-color;
+      color: $primary-color;
+      padding-bottom: 0vw;
+    }
+  }
+  // tab结束
 }
 </style>
