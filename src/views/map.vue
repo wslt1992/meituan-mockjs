62519b6c1e46e92e5d621ae66342ce44320d4a56
liu tao <465275024@qq.com>
2018-11-20 17:04:44 +0800

    map加载bug修改。

--- src/views/map.vue
+++ src/views/map.vue
@@ -8,28 +8,32 @@
     export default {
         data() {
             return {
-                amapKey: this.$global.key
+                amapKey: this.$global.key,
+                map:null,
+                onMapLoad:'onMapLoad'//地图加载完成，回调的函数
+            }
+        },
+        methods: {
+            initMap() {
+                window[this.onMapLoad]  = function(){
+                    let map = this.map = new AMap.Map('container',{
+                        zoom:10,
+                        // center:[116.39,39.9]
+                    });
+                    AMap.plugin('AMap.ToolBar',function(){//异步加载插件
+                        var toolbar = new AMap.ToolBar();
+                        map.addControl(toolbar);
+                    });
+                }
+                var url = `https://webapi.amap.com/maps?v=1.4.11&key=${this.key}值&callback=${this.onMapLoad}`;
+                var jsapi = document.createElement('script');
+                jsapi.charset = 'utf-8';
+                jsapi.src = url;
+                document.head.appendChild(jsapi);
             }
         },
         mounted(){
-            window.onLoad  = function(){
-                var map = new AMap.Map('container');
-            }
-            var url = `https://webapi.amap.com/maps?v=1.4.11&key=${this.key}值&callback=onLoad`;
-            var jsapi = document.createElement('script');
-            jsapi.charset = 'utf-8';
-            jsapi.src = url;
-            document.head.appendChild(jsapi);
-
-
-            var map = new AMap.Map('container',{
-                zoom:12,
-                center:[116.39,39.9]
-            });
-            AMap.plugin('AMap.ToolBar',function(){//异步加载插件
-                var toolbar = new AMap.ToolBar();
-                map.addControl(toolbar);
-            });
+            this.initMap();
         }
     }
 </script>
