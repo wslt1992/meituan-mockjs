f2032d0dd71877d5ac79bf96a99ce64b61c6e2cb
liu tao <465275024@qq.com>
2018-11-29 12:44:09 +0800

    添加shop外观

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <t-right-menu >
+    <t-page>
+        
+        <t-right-menu slot="container">
             <div slot="left" class="menu-left">
                 <p left-item>口味选择</p>
                 <p left-item>烤肉类</p>
@@ -11,17 +12,81 @@
                 <p left-item>酒水类</p>
             </div>
             <div  slot='right' class="menu-right">
-                <div right-item >
-                    <p >鸡脆骨</p>
-                    <p >烤韭菜</p>
-                    <p >羊肉串</p>
-                    <p >鸡全翅</p>
-                    <p >葱香豆皮</p>
-                    <p >谈烤肥牛</p>
-                    <p >葱香茄子</p>
-                    <p >台湾热狗</p>
+                <div right-item  default-show>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">鸡脆骨</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">烤韭菜</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">羊肉串</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">鸡全翅</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">葱香豆皮</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">谈烤肥牛</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">葱香茄子</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
+                    <div class="food-item">
+                        <img src="../assets/imgs/food1.jpg" alt="">
+                        <div>
+                            <span class="header">台湾热狗</span>
+                            <span class="tips">够脆、够香、嘎嘣脆</span>
+                            <div><span class="sale">月销量：5435</span><span class="like">赞23</span></div>
+                            <span class="price">￥23</span>
+                        </div>
+                    </div>
                 </div>
-                <div right-item default-show>
+                <div right-item >
                     <p >羊肉串</p>
                     <p >蜜汁牛排</p>
                     <p >滋补羊腰</p>
@@ -85,7 +150,7 @@
                 </div>
             </div>
         </t-right-menu>
-    </div>
+    </t-page>
 </template>
 
 <script>
@@ -97,14 +162,52 @@
 <style lang='scss' scoped>
 // 左右菜单开始
 .menu-left{
-    width: 20%;
+    width: 25%;
     max-height: 100vh;
     overflow: scroll;
+    [left-item]{
+        padding: 3vw 0;
+    }
 }
 .menu-right{
-    width: 80%;
+    text-align: start;
+    width: 75%;
     max-height: 100vh;
     overflow: scroll;
+    color: #555;
+    padding-left: 3vw;
+    .food-item{
+        display: flex;
+        margin-bottom: 4vw;
+        >img{
+            display: block;
+            width: 22.6vw;
+            height: 22.6vw;
+        }
+        >div{
+            padding-left: 3vw;
+        }
+        .header{
+            color: #555;
+            font-size: 5vw;
+            font-weight: bold;
+        }
+        .tips{
+            color: #aaa;
+            display: block;
+            font-size: 3.6vw;
+        }
+        .sale , .like{
+            font-size: 3.6vw;
+            color: #aaa;
+        }
+        .price{
+            color: #ff4c41;
+            font-weight: 700;
+            font-size: 5vw;
+        }
+    }
+    
 }
 // 左右菜单结束
 </style>
