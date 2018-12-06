4f18e8e453ae63269e2026c4f8050d7cc300a8b5
liu tao <465275024@qq.com>
2018-12-06 12:57:10 +0800

    t-page的根加上宽100vw;

--- src/components/t-page.vue
+++ src/components/t-page.vue
@@ -38,6 +38,7 @@
     display: flex;
     flex-direction: column;
     height: 100%;
+    width: 100vw;
     // height: 100vh;//不可以设置，设置后，部分也没有100vh高。让父为100vh高，所以设置app的改的呀
     .header{
         width: 100%;
