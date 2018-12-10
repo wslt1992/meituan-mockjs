23c356ac2844c1fc349a067883521e440d8b397b
liu tao <465275024@qq.com>
2018-12-10 17:59:34 +0800

    加入babel-polyfill兼容包，ie

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// ie兼容包
+// import "babel-polyfill";
 import Vuex from 'vuex'
 import App from './App'
 import router from './router'
